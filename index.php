bb244727cdcbfa800d9f415e57ab17ece67510db
yassine elhanfaoui <60264377+yassine106@users.noreply.github.com>
2020-04-29 04:52:21 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php 
+    include'traiter.php';
+    ?>
+<!DOCTYPE html>
+<html>
+<head>
+ <title> Login Form in HTML5 and CSS3</title>
+<link rel="stylesheet" type="text/css" href="css/util.css">
+ <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+ <link rel="stylesheet" type="text/css" href="css/main.css">
+
+
+</head>
+<body>
+ 
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/img-07.jpg');">
+			<div class="wrap-login100 p-t-190 p-b-30">
+				<form class="login100-form validate-form" action="traiter.php" method="POST">
+					<div class="login100-form-avatar">
+						
+					</div>
+					<div class="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-10" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock"></i>
+						</span>
+					</div>
+                                    <p>
+                                        <?php         
+                                        if (isset($message)){
+                                        echo $message;}
+                                        ?>
+                                    </p>
+
+					<div class="container-login100-form-btn p-t-10">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					
+
+										
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+        <script src="jquery/jquery-3.2.1.min.js"></script>
+      < <script src="js/main.js"></script>
+<script src="bootstrap/js/popper.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ traiter.php
@@ -0,0 +1,50 @@
+
+<?php
+$server='127.0.0.1';
+$user="root";
+$password='';
+$datbase='login';
+$connect= mysqli_connect($server,$user,$password,$datbase);
+if (mysqli_connect_errno()) {
+    die("cannot connect to database".mysqli_connect_errno());
+}       
+ else {
+echo "Database is connected";    
+}
+?>
+<?php
+
+if(isset($_POST['username'])){
+    
+    $uname=$_POST['username'];
+    $password=$_POST['pass'];
+    
+    $sql="select * from user where username='".$uname."'AND password='".$password."' limit 1";
+    
+    $result= mysqli_query($connect,$sql);
+    if(mysqli_num_rows($result)==1){
+        $type=mysqli_fetch_row($result);
+        if( $type[3] == 'user'){
+            header("Location: user.php");
+            
+        }
+        else{
+            header("Location: admin.php");
+        }
+        
+        
+        exit();
+        
+    }
+    else{
+      header("Location: index.php");
+        exit();
+    }
+    
+    
+        
+}
+?>
+<?php
+mysqli_close($connect);
+?>
